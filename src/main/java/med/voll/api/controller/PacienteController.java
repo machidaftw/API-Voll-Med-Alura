5f286fc55285e168659a5b8195b0f23a50060c3e
machidaftw <machidaftw1@hotmail.com>
2024-06-17 01:34:40 -0300

    Adicionando funcionalidade para atualizar médico e paciente no banco de dados

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,8 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosListagemPaciente;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.medico.DadosAtualizacaoMedico;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,4 +27,13 @@ public class PacienteController {
     public Page<DadosListagemPaciente> Listar(@PageableDefault(size = 10, sort = "nome") Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+
 }
